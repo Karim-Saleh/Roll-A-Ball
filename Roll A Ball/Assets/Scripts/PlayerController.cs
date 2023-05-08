508cd1499547a91770bcc7cf99f2be0d6dfc3bc6
Karim Saleh <karimsaleha@outlook.com>
2023-05-08 22:11:58 +0300

    Created The Game Till Player Movements
    -Created ground and player ball.
    -Created 2 materials.
    -Added physics to the ball.
    -Added player controls.

---
+++ Roll A Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX, movementY;
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+}
