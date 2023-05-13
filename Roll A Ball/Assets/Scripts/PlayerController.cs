b8b6d04f19d258cd70de0a398c6f859d045381e1
Mostafa Mohamed <mostafamohamed123345@yahoo.com>
2023-05-13 16:58:14 +0300

    Added Trigger Zones and Score UI

--- Roll A Ball/Assets/Scripts/PlayerController.cs
+++ Roll A Ball/Assets/Scripts/PlayerController.cs
@@ -4,23 +4,29 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     private float movementX, movementY;
     public float speed = 10.0f;
+    private int count;
+    public TextMeshProUGUI countText;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCount();
     }
 
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
+        SetCount();
     }
 
     private void OnMove(InputValue movementValue)
@@ -28,5 +34,20 @@ public class PlayerController : MonoBehaviour
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    private void SetCount() 
+    {
+        countText.text = "Player Score: " + count.ToString();
+    }
+
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("pickups")) 
+        {
+            count ++;
+            other.gameObject.SetActive(false);
+        }
     }
 }
